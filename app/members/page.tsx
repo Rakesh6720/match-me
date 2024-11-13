0c922c3e0257a8792fe3b02dcb50c59459040b45
Rakesh Baruah <rbaruah@crisisprevention.com>
2024-11-13 16:39:53 -0600

    feat: create MemberCard component; update MembersPage to display members using MemberCard

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { getMembers } from "../actions/memberActions";
+import MemberCard from "./MemberCard";
 
 const MembersPage = async () => {
   const members = await getMembers();
   return (
     <div>
-      {members && members.map(({ name, id }) => <p key={id}>{name}</p>)}
+      {members &&
+        members.map((member) => <MemberCard key={member.id} member={member} />)}
     </div>
   );
 };
