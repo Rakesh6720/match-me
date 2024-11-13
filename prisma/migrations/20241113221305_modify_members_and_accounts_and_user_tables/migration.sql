a94c2a0ce59d2db5104f6125a961faa5ae28e2f6
Rakesh Baruah <rbaruah@crisisprevention.com>
2024-11-13 16:13:20 -0600

    feat: update database schema; add memberId to User model and establish foreign key relationships for Account and Member models

---
+++ prisma/migrations/20241113221305_modify_members_and_accounts_and_user_tables/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "memberId" TEXT;
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Member" ADD CONSTRAINT "Member_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
