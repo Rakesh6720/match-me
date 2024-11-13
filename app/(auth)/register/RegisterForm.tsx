1d30ba36f18917f3824d4b49add80600e1df2950
Rakesh Baruah <rbaruah@crisisprevention.com>
2024-11-13 14:17:42 -0600

    refactor registration form; simplify structure, remove multi-step logic, and enhance input handling

--- app/(auth)/register/RegisterForm.tsx
+++ app/(auth)/register/RegisterForm.tsx
@@ -1,112 +1,81 @@
 "use client";
 
-import {
-  profileSchema,
-  registerSchema,
-  RegisterSchema,
-} from "@/lib/schemas/registerSchema";
-
-import { Card, CardHeader, CardBody, Button } from "@nextui-org/react";
-import React, { useState } from "react";
-import { FormProvider, useForm } from "react-hook-form";
-import { GiPadlock } from "react-icons/gi";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
 import { registerUser } from "@/app/actions/authActions";
+import { registerSchema, RegisterSchema } from "@/lib/schemas/registerSchema";
 import { handleFormServerErrors } from "@/lib/util";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Card, CardBody, CardHeader, Input } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
 
-const stepSchemas = [registerSchema, profileSchema];
-
-export default function RegisterForm() {
-  const [activeStep, setActiveStep] = useState(0);
-  const currentValidationSchema = stepSchemas[activeStep];
-
-  const registerFormMethods = useForm<RegisterSchema>({
-    resolver: zodResolver(currentValidationSchema),
-    mode: "onTouched",
-  });
-
+const RegisterForm = () => {
   const {
+    register,
     handleSubmit,
     getValues,
-    setError,
     formState: { errors, isValid, isSubmitting },
-  } = registerFormMethods;
+  } = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+    mode: "onTouched",
+  });
 
   const router = useRouter();
 
   const onSubmit = async () => {
     const result = await registerUser(getValues());
     if (result.status === "success") {
-      router.push("/register/success");
+      router.push("/members");
     } else {
-      handleFormServerErrors(result, setError);
-    }
-  };
-
-  const getStepContent = (step: number) => {
-    switch (step) {
-      default:
-        return "Unknown step";
-    }
-  };
-
-  const onBack = () => {
-    setActiveStep((prev) => prev - 1);
-  };
-
-  const onNext = async () => {
-    if (activeStep === stepSchemas.length - 1) {
-      await onSubmit();
-    } else {
-      setActiveStep((prev) => prev + 1);
+      console.log(result.error);
     }
   };
 
   return (
     <Card className="w-3/5 mx-auto">
-      <CardHeader className="flex flex-col items-center justify-center">
-        <div className="flex flex-col gap-2 items-center text-default">
-          <div className="flex flex-row items-center gap-3">
-            <GiPadlock size={30} />
-            <h1 className="text-3xl font-semibold">Register</h1>
-          </div>
-          <p className="text-neutral-500">Welcome to NextMatch</p>
-        </div>
-      </CardHeader>
+      <CardHeader className="flex flex-col items-center justify-center"></CardHeader>
       <CardBody>
-        <FormProvider {...registerFormMethods}>
-          <form onSubmit={handleSubmit(onNext)}>
-            <div className="space-y-4">
-              {getStepContent(activeStep)}
-              {errors.root?.serverError && (
-                <p className="text-danger text-sm">
-                  {errors.root.serverError.message}
-                </p>
-              )}
-              <div className="flex flex-row items-center gap-6">
-                {activeStep !== 0 && (
-                  <Button onClick={onBack} fullWidth>
-                    Back
-                  </Button>
-                )}
-                <Button
-                  isLoading={isSubmitting}
-                  isDisabled={!isValid}
-                  fullWidth
-                  color="default"
-                  type="submit"
-                >
-                  {activeStep === stepSchemas.length - 1
-                    ? "Submit"
-                    : "Continue"}
-                </Button>
-              </div>
-            </div>
-          </form>
-        </FormProvider>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="space-y-4">
+            <Input
+              defaultValue=""
+              label="Name"
+              variant="bordered"
+              {...register("name")}
+              isInvalid={!!errors.name}
+              errorMessage={errors.name?.message}
+            />
+            <Input
+              defaultValue=""
+              label="Email"
+              variant="bordered"
+              {...register("email")}
+              isInvalid={!!errors.email}
+              errorMessage={errors.email?.message}
+            />
+            <Input
+              defaultValue=""
+              label="Password"
+              variant="bordered"
+              type="password"
+              {...register("password")}
+              isInvalid={!!errors.password}
+              errorMessage={errors.password?.message}
+            />
+            <Button
+              isLoading={isSubmitting}
+              disabled={!isValid}
+              fullWidth
+              color="default"
+              type="submit"
+            >
+              Register
+            </Button>
+          </div>
+        </form>
       </CardBody>
     </Card>
   );
-}
+};
+
+export default RegisterForm;
