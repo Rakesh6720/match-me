c496e72f7ff25fa1f88e60208ab3dad6a39a85a9
Rakesh Baruah <rbaruah@crisisprevention.com>
2024-11-14 11:55:06 -0600

    feat: add LikeButton component and integrate it into MemberCard; adjust grid layout in MembersPage

--- app/members/MemberCard.tsx
+++ app/members/MemberCard.tsx
@@ -5,6 +5,7 @@ import { Card, CardFooter, Image } from "@nextui-org/react";
 import { Member } from "@prisma/client";
 import Link from "next/link";
 import React from "react";
+import LikeButton from "../components/LikeButton";
 
 type Props = {
   member: Member;
@@ -20,6 +21,9 @@ export default function MemberCard({ member }: Props) {
         src={member.image || "/images/user.png"}
         className="aspect-square object-cover"
       />
+      <div className="absolute top-3 right-3 z-50">
+        <LikeButton targetId={member.userId} hasLiked={false} />
+      </div>
       <CardFooter className="flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient">
         <div className="flex flex-col text-white">
           <span className="font-semibold">
