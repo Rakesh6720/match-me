b0959f3c90b37bf431787c1333a1f8036a8bab0f
Rakesh Baruah <rbaruah@crisisprevention.com>
2024-11-14 15:05:31 -0600

    feat: implement ListsTab component to display liked members with tab navigation

--- app/lists/page.tsx
+++ app/lists/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import {
+  fetchCurrentUserLikeIds,
+  fetchLikedMembers,
+} from "../actions/likeActions";
+import ListsTab from "./ListsTab";
+import { Member } from "@prisma/client";
 
-const ListsPage = () => {
-  return <div>ListsPage</div>;
+const ListsPage = async ({
+  searchParams,
+}: {
+  searchParams: { type: string };
+}) => {
+  const likeIds = await fetchCurrentUserLikeIds();
+  const members = (await fetchLikedMembers(searchParams.type)) as Member[];
+
+  return (
+    <div>
+      <ListsTab members={members} likeIds={likeIds} />
+    </div>
+  );
 };
 
 export default ListsPage;
