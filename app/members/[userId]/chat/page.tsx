0dd1463f49e87b6a245a6a40ca2717d36c3d8101
Rakesh Baruah <rbaruah@gmail.com>
2024-11-15 14:51:16 -0600

    feat: implement chat functionality with message display and user interaction

--- app/members/[userId]/chat/page.tsx
+++ app/members/[userId]/chat/page.tsx
@@ -1,8 +1,35 @@
 import CardInnerWrapper from "@/app/components/CardInnerWrapper";
 import React from "react";
+import ChatForm from "./ChatForm";
+import { getMessageThread } from "@/app/actions/messageActions";
+import MessageBox from "./MessageBox";
+import { getAuthUserId } from "@/app/actions/authActions";
 
-const ChatPage = () => {
-  return <CardInnerWrapper header="Chat" body="Chat goes here" />;
-};
+export default async function ChatPage({
+  params,
+}: {
+  params: { userId: string };
+}) {
+  const messages = await getMessageThread(params.userId);
+  const userId = await getAuthUserId();
 
-export default ChatPage;
+  const body = (
+    <div>
+      {messages.length === 0 ? (
+        "No messages to display"
+      ) : (
+        <div>
+          {messages.map((message) => (
+            <MessageBox
+              key={message.id}
+              message={message}
+              currentUserId={userId}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+
+  return <CardInnerWrapper header="Chat" body={body} footer={<ChatForm />} />;
+}
