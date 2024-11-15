6335fcff76f0bed009b75be925e0e8944d3c909d
Rakesh Baruah <rbaruah@gmail.com>
2024-11-15 07:08:06 -0600

    feat: add EditForm component and memberEditSchema for member profile editing

---
+++ app/members/edit/EditForm.tsx
@@ -0,0 +1,11 @@
+import { Member } from "@prisma/client";
+import React from "react";
+
+type Props = {
+  member: Member;
+};
+const EditForm = ({ member }: Props) => {
+  return <div>EditForm</div>;
+};
+
+export default EditForm;
